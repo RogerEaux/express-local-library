5def1b3d49ee833706c819832e57b80e13866d5f
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-12 01:05:38 -0600

    Fix catalog router missing slash

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('catalog', catalogRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
