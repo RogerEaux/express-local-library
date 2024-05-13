d7986f974361f9807316946281f187f4937077bb
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-12 19:19:22 -0600

    Add genre detail controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 import Genre from '../models/genre.js';
+import Book from '../models/book.js';
 import asyncHandler from 'express-async-handler';
 
 export const genreList = asyncHandler(async (req, res, next) => {
@@ -7,8 +8,23 @@ export const genreList = asyncHandler(async (req, res, next) => {
   res.render('genreList', { title: 'Genre List', genre_list: allGenres });
 });
 
-export const genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+export const genreDetail = asyncHandler(async (req, res, next) => {
+  const [genre, genreBooks] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, 'title summary').exec(),
+  ]);
+
+  if (!genre) {
+    const err = new Error('Genre not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('genreDetail', {
+    title: 'Genre Detail',
+    genre: genre,
+    genre_books: genreBooks,
+  });
 });
 
 export const genre_create_get = asyncHandler(async (req, res, next) => {
